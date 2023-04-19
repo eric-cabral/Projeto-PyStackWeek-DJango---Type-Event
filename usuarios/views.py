3c1f8995f0a6064162dbf8019557d0e8ab1b36a7
Eric Cabral <eric2509.cabral@gmail.com>
2023-04-18 23:57:54 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
+
+# Create your views here.
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não são iguais!')
+            return redirect(reverse('cadastro'))
+        
+        # TODO: Validar força da senha
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já cadastrado!')
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'CADASTRO REALIZADO COM SUCESSO!!!')
+
+        return redirect(reverse('login'))
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
