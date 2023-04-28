d920b10feb015d9a54f818032e16747de5083a29
Eric Cabral <eric2509.cabral@gmail.com>
2023-04-27 21:33:55 -0300

    participantesEventoCompleto

--- eventos/views.py
+++ eventos/views.py
@@ -6,6 +6,10 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.http import Http404
+import csv
+from secrets import token_urlsafe
+import os
+from django.conf import settings
 
 @login_required
 def novo_evento(request):
@@ -78,6 +82,22 @@ def participantes_evento(request, id):
     if request.method == 'GET':
         participantes = evento.participantes.all()
         return render(request, 'participantes_evento.html', {'participantes': participantes, 'evento': evento})
-    elif request.method == 'POST':
-        pass
-    ### PAREI NO TEMPO 01:41:00 DA AULA 02
+
+
+def gerar_csv(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    
+    participantes = evento.participantes.all()
+
+    token = f'{token_urlsafe(6)}.csv'
+    path = os.path.join(settings.MEDIA_ROOT, token)
+
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=';')
+        for participante in participantes:
+            x = (participante.username, participante.email)
+            writer.writerow(x)
+
+    return redirect(f'/media/{token}')
